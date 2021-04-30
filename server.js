b93d9583b1710e1f1dae78646aa277b9ea9b4829
Stephen Luu <61605603+PhtephenLuu@users.noreply.github.com>
2021-04-30 15:08:23 -0400

    Fix file structure and add dockerfile

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ const pug = require("pug");
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = 8080;
 
-app.set("views", "./");
+app.set("views", "./views");
 app.set("view engine", "pug");
 app.use(express.json());
 
 //Home page
 app.get("/", (req, res) => {
+  var getString;
+  if (req.query.Title)
+    res.send(
+      "http://www.omdbapi.com/?i=" + req.query.Title + "&apikey=e8f391fc"
+    );
+
   res.status(200);
   res.render("home.pug");
 });
